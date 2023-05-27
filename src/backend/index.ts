d3fd061be4e146a9e6635cb317baecbdb8f7d46c
AkitoTobita <t0bitakito.001@gmail.com>
2023-05-26 19:36:27 -0700

    chore:added mongodb connection

---
+++ src/backend/index.ts
@@ -0,0 +1,22 @@
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+require("dotenv").config({ path: path.resolve(__dirname, ".", ".env") });
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+mongoose.connect(`${process.env.MONGO_URI}`, { useNewUrlParser: true });
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", () => console.error("connected to database"));
+
+app.listen(port, () => {
+  console.log("server is running");
+});
