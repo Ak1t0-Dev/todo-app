38108a927d392101b7793fd189c8bdb061bc4469
AkitoTobita <t0bitakito.001@gmail.com>
2023-05-31 14:18:26 -0700

    style: adjusted simple layout to a home page

--- backend/controllers/usersController.ts
+++ backend/controllers/usersController.ts
@@ -2,7 +2,15 @@ import { Request, Response } from "express";
 import Users from "../models/users";
 
 export const getUserPosts = async (req: Request, res: Response) => {
-  const userpostsList = await Users.find().populate("posts");
-  console.log(userpostsList);
-  res.json(userpostsList);
+  // const user = await Users.find().populate("posts");
+  const user = await Users.find().populate({
+    path: "posts",
+    populate: [
+      { path: "categories", model: "Categories" },
+      { path: "tags", model: "Tags" },
+    ],
+  });
+  const posts = user.flatMap((user) => user.posts);
+  console.log(posts);
+  res.json(posts);
 };
--- src/views/pages/Home/Home.tsx
+++ src/views/pages/Home/Home.tsx
@@ -1,35 +1,36 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { ObjectId } from "mongoose";
+import styled from "styled-components";
 
-interface User {
-  username: string;
-  password: string;
-  firstname: string;
-  lastname: string;
-  address: string;
-  post: Post[];
-}
+// interface User {
+//   username: string;
+//   password: string;
+//   firstname: string;
+//   lastname: string;
+//   address: string;
+//   post: Post[];
+// }
 
 interface Post {
-  id: ObjectId;
+  _id: ObjectId;
   title: string;
   categories: Category[];
   tags: Tag[];
 }
 
 interface Category {
-  id: ObjectId;
+  _id: ObjectId;
   category: string;
 }
 
 interface Tag {
-  id: ObjectId;
+  _id: ObjectId;
   tag: string;
 }
 
 export const Home = () => {
-  const [posts, setPosts] = useState<User[]>([]);
+  const [userPosts, setUserPosts] = useState<Post[]>([]);
   const [categories, setCategories] = useState<Category[]>([]);
   const [tags, setTags] = useState<Tag[]>([]);
 
@@ -44,8 +45,7 @@ export const Home = () => {
       .get("http://localhost:3001/api/userposts")
       .then((response) => {
         const data = response.data;
-        console.log(data);
-        setPosts(data);
+        setUserPosts(data);
       })
       .catch((err) => {
         console.error("err:", err);
@@ -76,14 +76,110 @@ export const Home = () => {
       });
   };
 
+  console.log("tags", tags);
+  console.log("categories", categories);
+  console.log("userPosts", userPosts);
+
   return (
-    <>
-      {categories.map((item, index) => {
-        return <div key={index.toString()}>{item.category}</div>;
-      })}
-      {tags.map((item, index) => {
-        return <div key={index.toString()}>{item.tag}</div>;
-      })}
-    </>
+    <Main>
+      <Categories>
+        {categories.map((item, index) => {
+          return <Item key={index.toString()}>{item.category}</Item>;
+        })}
+      </Categories>
+      <Tags>
+        {tags.map((item, index) => {
+          return <Item key={index.toString()}>{item.tag}</Item>;
+        })}
+      </Tags>
+      <PostsList>
+        {userPosts.map((post, index) => {
+          return (
+            <Posts key={index.toString()}>
+              <div>{post.title}</div>
+              <PostCategories>
+                {post.categories &&
+                  post.categories.map((item, index) => {
+                    return <PostItem key={index}>{item.category}</PostItem>;
+                  })}
+              </PostCategories>
+              <PostTags>
+                {post.tags &&
+                  post.tags.map((item, index) => {
+                    return <PostItem key={index}>{item.tag}</PostItem>;
+                  })}
+              </PostTags>
+            </Posts>
+          );
+        })}
+      </PostsList>
+    </Main>
   );
 };
+
+const Main = styled.div`
+  padding: 1rem;
+`;
+
+const PostsList = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  gap: 1rem;
+`;
+
+const Posts = styled.div`
+  border: 3px solid black;
+  width: 300px;
+  padding: 1rem;
+`;
+
+const Categories = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  gap: 0.3rem;
+  border: 3px solid black;
+  padding: 1rem;
+  margin: 1rem 0;
+`;
+
+const Tags = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  gap: 0.3rem;
+  border: 3px solid black;
+  padding: 1rem;
+  margin: 1rem 0;
+`;
+
+const Item = styled.span`
+  border: 1px solid black;
+  border-radius: 1rem;
+  padding: 0.3rem 0.5rem;
+  font-size: 1rem;
+`;
+
+const PostCategories = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  gap: 0.3rem;
+  padding: 0.3rem;
+`;
+
+const PostTags = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  gap: 0.3rem;
+  padding: 0.3rem;
+`;
+
+const PostItem = styled.span`
+  border: 1px solid black;
+  border-radius: 1rem;
+  padding: 0.3rem 0.5rem;
+  font-size: 0.5rem;
+`;
